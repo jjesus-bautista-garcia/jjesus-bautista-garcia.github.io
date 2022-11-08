ca23ed2ca6120b33c86ebaf1eef0bc5fdbaa94a8
jjesusbautista <jesus.bautista@pirelli.com>
2022-11-07 22:30:38 -0600

    working with just one variable to render

--- app/routes.py
+++ app/routes.py
@@ -4,14 +4,17 @@ from app import app
 from flask import render_template
 ## other
 from app.bookstore.functions.main import getHeaderJson
+import os
+
+actualFilename = os.path.basename(__file__)
 
 @app.route('/')
 @app.route('/index')
 def index():
     # JSON base
     base_dict = getHeaderJson()
-    title = base_dict["header"]['title']
+    print(f'{actualFilename}-variable - base_dict: {base_dict}')
 
-    return render_template('index.html', title=title, base_dict=base_dict)
+    return render_template('index.html', base_dict=base_dict)
 ####
 ####
