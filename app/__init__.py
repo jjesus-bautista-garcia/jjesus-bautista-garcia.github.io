faab248692a43ab15c50500d81f1d0eedf594af5
jjesusbautista <jesus.bautista@pirelli.com>
2022-11-08 17:47:54 -0600

    trying to publish static page

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from app.config import Config
-
-app = Flask(__name__)
-app.config.from_object(Config)
-app.config['JSON_AS_ASCII'] = False
-
-from app import routes
--- app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    username = StringField(required="username", validators=[DataRequired()])
-    password = PasswordField(required="password", validators=[DataRequired()])
-    rememberMe = BooleanField(required="rememberMe")
-    submit = SubmitField("Sign In")
