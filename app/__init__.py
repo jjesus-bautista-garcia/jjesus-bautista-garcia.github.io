ee490ee8dc6030fbc4d0626b8a53378dbb0d4ddc
jjesusbautista <jesus.bautista@pirelli.com>
2022-11-07 22:30:02 -0600

    trying to fix utf-8

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from config import Config
+from app.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config['JSON_AS_ASCII'] = False
 
 from app import routes
--- app/bookstore/functions/main.py
+++ app/bookstore/functions/main.py
@@ -2,9 +2,11 @@ import json
 
 def readJSON(path, mode):
     with open(path, mode) as f:
-            base_dict = json.loads(f.read())
+        #     base_dict = f.read()
+            base_dict = json.loads(f.read().decode('utf-8'))
+        #     base_dict = json.dumps(f.read())
             f.close()
     return base_dict
 
 def getHeaderJson():
-        return readJSON("App/static/data/base.json", "rt")
+        return readJSON("App/static/data/base.json", "rb")
