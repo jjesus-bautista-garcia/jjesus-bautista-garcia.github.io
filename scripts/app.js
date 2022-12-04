297b8f2dbb88f23c3487c0a166559064c2da1bb1
jjesusbautista <jesus.bautista@pirelli.com>
2022-12-04 17:32:45 -0600

    using DOM for inject data, and add gmail

--- scripts/app.js
+++ scripts/app.js
@@ -2,19 +2,39 @@
 let appendBodyData = data => {
     console.log('base json: ', data)
     
-    document.querySelector('title')
+    innerDescription(data)
+
+    innerBody(data)
+};
+// / inner description data to DOM
+let innerDescription = data => {
+  document.querySelector('title')
       .append(data.meta.title)
 
-    document.querySelector('#descHead')
-      .append(data.main.information.description.head)
+  document.querySelector('#descHead')
+    .append(data.main.information.description.head)
 
-    document.querySelector('#descSubhead')
-      .append(data.main.information.description.subhead)
+  document.querySelector('#descSubhead')
+    .append(data.main.information.description.subhead)
+} 
 
-    document.querySelector('#descBody')
-      .append(data.main.information.description.body)
-};
+let innerBody = data => {
+  const bodyId = "descBody";
+  const bodyDataObject = data.main.information.description.body;
+  
+  for (const [key, value] of Object.entries(bodyDataObject)) {
+    let bodyElement = document.getElementById(bodyId);
+    let elementNode = document.createElement('p');
+    let textNode = document.createTextNode(`${value}`);
 
+    
+    elementNode.setAttribute('id', bodyId+'Child');
+    elementNode.appendChild(textNode);
+    bodyElement.appendChild(elementNode);
+
+    console.log(`${key} - ${value} `)
+  };
+}
 // Download base json file
  
  fetch('./data/base.json')
